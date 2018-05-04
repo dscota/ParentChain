2015ce4e126393a09b59e7d472a394f98b44596e
Anthony Rosario <anthonyrosario@dhcp-18-189-42-128.dyn.MIT.EDU>
2018-05-04 08:32:17 -0400

    dogtor almost done

--- js/profile.js
+++ js/profile.js
@@ -1,14 +1,16 @@
 var followers = {"dogtor": 67};
 var following = {"dogtor": 21};
-var followed = [];
+var isFollowing = false;
 
-function follow_click(profile){
-  if (followed.includes(profile) {
+function follow_click(){
+  if (isFollowing) {
     followers[profile] -= 1;
+    isFollowing = false;
     //change follow button
   }
   else {
     followers[profile] += 1;
+    isFollowing = true;
     //change follow button
   }
 }
